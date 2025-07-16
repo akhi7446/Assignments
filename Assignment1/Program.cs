9cf63a661a79cba5f69a98585ae08a4f4538dd9a
akhi7445 <aakashdurgapu66@gmail.com>
2025-07-16 16:59:22 +0530

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,181 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        //{
+        //    Console.Write("Enter first number: ");
+        //    int a = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.Write("Enter second number: ");
+        //    int b = Convert.ToInt32(Console.ReadLine());
+
+        //    int sum = a + b;
+        //    int difference = a - b;
+        //    int product = a * b;
+        //    double quotient = (double)a / b; // cast to show decimal
+
+        //    Console.WriteLine($"Sum = {sum}");
+        //    Console.WriteLine($"Difference = {difference}");
+        //    Console.WriteLine($"Product = {product}");
+        //    Console.WriteLine($"Quotient = {quotient}");
+        //}
+
+
+
+        //{
+        //    Console.Write("Enter first number: ");
+        //    int a = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.Write("Enter second number: ");
+        //    int b = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.Write("Enter operator: ");
+        //    int ch = Convert.IoInt16(Console.ReadLine());
+
+        //    switch (ch)
+        //    {
+        //        case 1:
+        //            Console.WriteLine("Sum = " + (a + b));
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("Difference = " + (a - b));
+        //            break;
+        //        case 3:
+        //            Console.WriteLine("Product = " + (a * b));
+        //            break;
+        //        case 4:
+        //            if (b != 0)
+        //                Console.WriteLine("Quotient = " + (a / b));
+        //            else
+        //                Console.WriteLine("Cannot divide by zero");
+        //            break;
+        //        default:
+        //            Console.WriteLine("Invalid operator");
+        //            break;
+        //    }
+        //}
+
+        //{
+        //    for (int i = 1,i <= 10 ,i++)
+        //    {
+        //        Console.WriteLine("aakash");
+        //    }
+        //}
+
+
+        //{
+        //    // For loop
+        //    Console.WriteLine("Even numbers using for loop:");
+        //    for (int i = 2; i <= 20; i += 2)
+        //        Console.WriteLine(i);
+
+        //    // While loop
+        //    Console.WriteLine("Even numbers using while loop:");
+        //    int j = 2;
+        //    while (j <= 20)
+        //    {
+        //        Console.WriteLine(j);
+        //        j += 2;
+        //    }
+
+        //    // Do-While loop
+        //    Console.WriteLine("Even numbers using do-while loop:");
+        //    int k = 2;
+        //    do
+        //    {
+        //        Console.WriteLine(k);
+        //        k += 2;
+        //    } while (k <= 20);
+        //}
+
+        //{
+        //    // For loop
+        //    Console.WriteLine("Odd numbers using for loop:");
+        //    for (int i = 1; i <= 19; i += 2)
+        //        Console.WriteLine(i);
+
+        //    // While loop
+        //    Console.WriteLine("Odd numbers using while loop:");
+        //    int j2 = 1;
+        //    while (j2 <= 19)
+        //    {
+        //        Console.WriteLine(j2);
+        //        j2 += 2;
+        //    }
+
+        //    // Do-While loop
+        //    Console.WriteLine("Odd numbers using do-while loop:");
+        //    int k2 = 1;
+        //    do
+        //    {
+        //        Console.WriteLine(k2);
+        //        k2 += 2;
+        //    } while (k2 <= 19);
+        //}
+
+
+
+        //{
+        //    Console.Write("Enter a number to display its table: ");
+        //    int num = Convert.ToInt32(Console.ReadLine());
+
+        //    // For loop
+        //    Console.WriteLine("\nTable using for loop:");
+        //    for (int i = 1; i <= 10; i++)
+        //    {
+        //        Console.WriteLine($"{num} x {i} = {num * i}");
+        //    }
+
+        //    // While loop
+        //    Console.WriteLine("\nTable using while loop:");
+        //    int j = 1;
+        //    while (j <= 10)
+        //    {
+        //        Console.WriteLine($"{num} x {j} = {num * j}");
+        //        j++;
+        //    }
+
+        //    // Do-while loop
+        //    Console.WriteLine("\nTable using do-while loop:");
+        //    int k = 1;
+        //    do
+        //    {
+        //        Console.WriteLine($"{num} x {k} = {num * k}");
+        //        k++;
+        //    } while (k <= 10);
+        //}
+
+        //{
+        //    Console.WriteLine("Numbers from 100 to 5 with a gap of 3:");
+
+        //    for (int i = 100; i >= 5; i -= 3)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+        //{
+        //    int a = 10, b = 20, c = 30;
+
+        //    Console.WriteLine($"Values are: a = {a}, b = {b}, c = {c}");
+        //}
+
+        //{
+        //    int a = 5;
+        //    int b = 10;
+        //    int c = 15;
+
+        //    Console.WriteLine("Value of a = " + a);
+        //    Console.WriteLine("Value of b = " + b);
+        //    Console.WriteLine("Value of c = " + c);
+        //}
+    }
+
+
+}
---
+++ assignment2/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace assignment2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Console.Write("Enter the marks of Math: ");
+            //int math = Convert.ToInt32 (Console.ReadLine());
+
+            //Console.Write("Enter the marks of Phy: ");
+            //int Phy = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter the marks of Chem: ");
+            //int Chem = Convert.ToInt32(Console.ReadLine());
+
+            //int total = math + Phy + chem;
+            //int MP = math + Phy;
+
+            //if(Math >= 65 && Phy >= 55 &&  Chem >=50 && total >= 180) || (MP >= 140)
+            //        {
+            //    Console.WriteLine("Eligible");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" Not Eligible");
+            //}
+
+
+            {
+                Console.Write("Customer IDNO: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Customer Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("unit consumed by the customer: ");
+                int units = Convert.ToInt32(Console.ReadLine());
+
+                double chargePerUnit;
+                double amount, surcharge = 0, netAmount;
+
+                if (units < 200)
+                    chargePerUnit = 1.20;
+                else if (units < 400)
+                    chargePerUnit = 1.50;
+                else if (units < 600)
+                    chargePerUnit = 1.80;
+                else
+                    chargePerUnit = 2.00;
+
+                amount = units * chargePerUnit;
+
+
+                if (amount > 400)
+                {
+                    surcharge = amount * 0.15;
+                }
+
+                netAmount = amount + surcharge;
+
+                // Apply minimum bill
+                if (netAmount < 100)
+                {
+                    netAmount = 100;
+                }
+
+                Console.WriteLine("Customer IDNO : " + id);
+                Console.WriteLine("Customer Name : " + name);
+                Console.WriteLine("Unit Consumed : " + units);
+                Console.WriteLine($"Amount Charges @Rs. {chargePerUnit} per unit : {amount}");
+                Console.WriteLine($"Surchage Amount : {surcharge}");
+                Console.WriteLine($"Net Amount Paid By the Customer : {netAmount}");
+            }
+        }
+
+    }
+}
