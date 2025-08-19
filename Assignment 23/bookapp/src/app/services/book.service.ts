02b209de5129ed90108efaa1a4adaa2dad4dffff
akhi7445 <aakashdurgapu66@gmail.com>
2025-08-19 11:14:25 +0530

    aa

---
+++ Assignment 23/bookapp/src/app/services/book.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private apiUrl = 'http://localhost:3000/books';
+
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+}
